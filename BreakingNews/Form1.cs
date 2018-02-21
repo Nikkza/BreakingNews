4db4caa7ed8ded8de34f7b9cdbe2216d021f1257
isaksson.nicklas80 <isaksson.nicklas80@outlook.com>
2018-02-21 22:43:04 +0100

    Made logic radiobuttons

--- BreakingNews/Form1.cs
+++ BreakingNews/Form1.cs
@@ -10,6 +10,8 @@ namespace BreakingNews
         private string _aftonbladet = "https://www.aftonbladet.se";
         private string _expressen = "https://www.expressen.se/";
         private string _dn = "https://www.dn.se/";
+        private const string _ekonomi = "ekonomi";
+        private string _changeurl;
 
         public Form1()
         {
@@ -18,100 +20,57 @@ namespace BreakingNews
             _myWebCollector = new WebCollector();
         }
         private async Task Loading()
-        {           
+        {
             labelCount.Text = "Loading...";
             await Task.Delay(1500);
         }
 
+        private void LogicRadioButton()
+        {
+            if (radioButtonAftonbladet.Checked)
+            {
+                _changeurl = _aftonbladet;
+            }
+            else if (radioButtonExpressen.Checked)
+            {
+                _changeurl = _expressen;
+            }
+            else if (radioButtonDN.Checked)
+            {
+                _changeurl = _dn;
+            }
+        }
+
         private async void buttonGetStat_Click(object sender, System.EventArgs e)
-        {                   
-            if (radioButtonAftonbladet.Checked == true && radioButtonAftonbladet.Text == "aftonbladet")
+        {
+            LogicRadioButton();
+
+            switch (this.groupBox1.SelectedRadioButton().Text)
             {
-                switch (this.groupBox1.SelectedRadioButton().Text)
-                {
-                    case "ekonomi":
-                        await Loading();
-                        await Task.Run(() => _myWebCollector.GetHtmlFromUrl(_aftonbladet));
-                        labelCount.Text = _myWebCalculator.CalculateNumberOfHits(_myWebCollector, radioButtonEkonomi.Text.ToLower()).ToString();
-                        return;
 
-                    case "polis":
-                        await Loading();
-                        await Task.Run(() => _myWebCollector.GetHtmlFromUrl(_aftonbladet));
-                        labelCount.Text = _myWebCalculator.CalculateNumberOfHits(_myWebCollector, radioButtonPolis.Text.ToLower()).ToString();
-                        return;
+                case "ekonomi":
+                    await Loading();
+                    await Task.Run(() => _myWebCollector.GetHtmlFromUrl(_changeurl));
+                    labelCount.Text = _myWebCalculator.CalculateNumberOfHits(_myWebCollector, this.groupBox1.SelectedRadioButton().Text).ToString();
+                    return;
 
-                    case "bostad":
-                        await Loading();
-                        await Task.Run(() => _myWebCollector.GetHtmlFromUrl(_aftonbladet));
-                        labelCount.Text = _myWebCalculator.CalculateNumberOfHits(_myWebCollector, radioButtonBostad.Text.ToLower()).ToString();
-                        return;
+                case "polis":
+                    await Loading();
+                    await Task.Run(() => _myWebCollector.GetHtmlFromUrl(_changeurl));
+                    labelCount.Text = _myWebCalculator.CalculateNumberOfHits(_myWebCollector, this.groupBox1.SelectedRadioButton().Text).ToString();
+                    return;
 
-                    case "korea":
-                        await Loading();
-                        await Task.Run(() => _myWebCollector.GetHtmlFromUrl(_aftonbladet));
-                        labelCount.Text = _myWebCalculator.CalculateNumberOfHits(_myWebCollector, radioButtonKorea.Text.ToLower()).ToString();
-                        return;
-                }
-            }
-            if (radioButtonExpressen.Checked == true && radioButtonExpressen.Text == "expressen")
-            {
-                switch (this.groupBox1.SelectedRadioButton().Text)
-                {
-                    case "ekonomi":
-                        await Loading();
-                        await Task.Run(() => _myWebCollector.GetHtmlFromUrl(_expressen));
-                        labelCount.Text = _myWebCalculator.CalculateNumberOfHits(_myWebCollector, radioButtonEkonomi.Text.ToLower()).ToString();
-                        return;
+                case "bostad":
+                    await Loading();
+                    await Task.Run(() => _myWebCollector.GetHtmlFromUrl(_changeurl));
+                    labelCount.Text = _myWebCalculator.CalculateNumberOfHits(_myWebCollector, this.groupBox1.SelectedRadioButton().Text).ToString();
+                    return;
 
-                    case "polis":
-                        await Loading();
-                        await Task.Run(() => _myWebCollector.GetHtmlFromUrl(_expressen));
-                        labelCount.Text = _myWebCalculator.CalculateNumberOfHits(_myWebCollector, radioButtonPolis.Text.ToLower()).ToString();
-                        return;
-
-                    case "bostad":
-                        await Loading();
-                        await Task.Run(() => _myWebCollector.GetHtmlFromUrl(_expressen));
-                        labelCount.Text = _myWebCalculator.CalculateNumberOfHits(_myWebCollector, radioButtonBostad.Text.ToLower()).ToString();
-                        return;
-
-                    case "korea":
-                        await Loading();
-                        await Task.Run(() => _myWebCollector.GetHtmlFromUrl(_expressen));
-                        labelCount.Text = _myWebCalculator.CalculateNumberOfHits(_myWebCollector, radioButtonKorea.Text.ToLower()).ToString();
-                        return;
-                }
-            }
-
-            if (radioButtonDN.Checked == true && radioButtonDN.Text == "dagensNyheter")
-            {
-                switch (this.groupBox1.SelectedRadioButton().Text)
-                {
-                    case "ekonomi":
-                        await Loading();
-                        await Task.Run(() => _myWebCollector.GetHtmlFromUrl(_dn));
-                        labelCount.Text = _myWebCalculator.CalculateNumberOfHits(_myWebCollector, radioButtonEkonomi.Text.ToLower()).ToString();
-                        return;
-
-                    case "polis":
-                        await Loading();
-                        await Task.Run(() => _myWebCollector.GetHtmlFromUrl(_dn));
-                        labelCount.Text = _myWebCalculator.CalculateNumberOfHits(_myWebCollector, radioButtonPolis.Text.ToLower()).ToString();
-                        return;
-
-                    case "bostad":
-                        await Loading();
-                        await Task.Run(() => _myWebCollector.GetHtmlFromUrl(_dn));
-                        labelCount.Text = _myWebCalculator.CalculateNumberOfHits(_myWebCollector, radioButtonBostad.Text.ToLower()).ToString();
-                        return;
-
-                    case "korea":
-                        await Loading();
-                        await Task.Run(() => _myWebCollector.GetHtmlFromUrl(_dn));
-                        labelCount.Text = _myWebCalculator.CalculateNumberOfHits(_myWebCollector, radioButtonKorea.Text.ToLower()).ToString();
-                        return;
-                }
+                case "korea":
+                    await Loading();
+                    await Task.Run(() => _myWebCollector.GetHtmlFromUrl(_changeurl));
+                    labelCount.Text = _myWebCalculator.CalculateNumberOfHits(_myWebCollector, this.groupBox1.SelectedRadioButton().Text).ToString();
+                    return;
             }
         }
     }
---
+++ NunitTests/TestClass.cs
@@ -0,0 +1,89 @@
+﻿using BreakingNews;
+using Castle.Components.DictionaryAdapter.Xml;
+using NUnit.Framework;
+
+namespace NunitTests
+{
+    [NUnit.Framework.TestFixture]
+    public class TestClass
+    {
+        /// <summary>
+        /// TestClass
+        /// </summary>
+
+        #region Here i started the test WebCalculatorTest 
+
+        [Test]
+        public void TestedEmptydUrl()
+        {
+            WebCollector wc = new WebCollector();
+            string s = "https://www.aftonbladet.se/";
+            wc.GetHtmlFromUrl(s);
+            if (s == string.Empty)
+            {
+                Assert.Fail();
+            }
+        }
+
+        [Test]
+        public void TestHttpUrl()
+        {
+            IWebCollector wc = new WebCollector();
+            string s = "https://www.aftonbladet.se/";
+            wc.GetHtmlFromUrl(s);
+            if (!s.StartsWith("https://"))
+            {
+                Assert.Fail();
+            }
+
+        }
+
+        [Test]
+        public void TestUnvalidUrl()
+        {
+            IWebCollector wc = new WebCollector();
+            string s = "https://www.aftonbladet.se/";
+            wc.GetHtmlFromUrl(s);
+            if (s.StartsWith("http://.."))
+            {
+                Assert.Fail();
+            }
+        }
+      
+        [Test]
+        public void TestNullObjekt()
+        {
+            IWebCollector wc = new WebCollector();
+            string s = "https://www.aftonbladet.se/";
+            wc.GetHtmlFromUrl(s);
+            if (s == null)
+            {
+                Assert.Fail();
+            }
+
+        }
+        #endregion
+
+        #region Here i started the test WebCalculatorTest
+
+        [Test]
+        public void TestNullObjectICalculator()
+        {
+            IWebCalculator wc = new WebCalculator();
+            IWebCollector wb = new WebCollector();
+            string s = "korea";
+            wb.GetHtmlFromUrl("https://www.aftonbladet.se/");
+            wc.CalculateNumberOfHits(wb, s);
+
+            if (wb == null && s == null)
+            {
+                Assert.Fail();
+            }
+        }
+
+
+        #endregion
+
+
+    }
+}
